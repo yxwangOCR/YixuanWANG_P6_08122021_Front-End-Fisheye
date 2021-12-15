1152834fb110111cb57dd150df43a24dddb92595
yxwangOCR <wang.yixuan@live.fr>
2021-12-15 15:30:29 +0100

    index & pages data media insert

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,39 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const location = document.createElement("location");
+    location.textContent = city + ", " + country;
+
+    const taglines = document.createElement("taglines");
+    taglines.textContent = tagline;
+
+    const prices = document.createElement("prices");
+    prices.textContent = price + " €";
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(taglines);
+    article.appendChild(prices);
+
+    return article;
+  }
+  return { name, picture, location, tagline, price, getUserCardDOM };
 }
+
+let imgOpen = document.querySelector("img");
+imgOpen.addEventListener("click", () => {
+  window.open("photographer.html", "_blank");
+  console.log("open links");
+});
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,132 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function getMedia() {
+  const media = [
+    {
+      id: 23523434,
+      photographerId: 243,
+      title: "Benevides Wedding",
+      image: "Event_BenevidesWedding.jpg",
+      likes: 77,
+      date: "2019-06-28",
+      price: 45,
+    },
+    {
+      id: 623534343,
+      photographerId: 243,
+      title: "Lonesome",
+      image: "Travel_Lonesome.jpg",
+      likes: 88,
+      date: "2019-02-03",
+      price: 45,
+    },
+    {
+      id: 95234343,
+      photographerId: 243,
+      title: "Rainbow Bird",
+      image: "Animals_Rainbow.jpg",
+      likes: 59,
+      date: "2019-07-02",
+      price: 60,
+    },
+    {
+      id: 65235234,
+      photographerId: 243,
+      title: "Boulder Wedding",
+      image: "Event_PintoWedding.jpg",
+      likes: 52,
+      date: "2019-06-25",
+      price: 45,
+    },
+    {
+      id: 2534342,
+      photographerId: 243,
+      title: "Seaside Wedding",
+      image: "Event_SeasideWedding.jpg",
+      likes: 25,
+      date: "2019-06-21",
+      price: 45,
+    },
+    {
+      id: 398847109,
+      photographerId: 243,
+      title: "Raw Black Portrait",
+      image: "Portrait_Background.jpg",
+      likes: 55,
+      date: "2019-06-20",
+      price: 45,
+    },
+    {
+      id: 2523434634,
+      photographerId: 243,
+      title: "Nora Portrait",
+      image: "Portrait_Nora.jpg",
+      likes: 63,
+      date: "2019-04-07",
+      price: 45,
+    },
+    {
+      id: 2525345343,
+      photographerId: 243,
+      title: "Wednesday Potrait",
+      image: "Portrait_Wednesday.jpg",
+      likes: 34,
+      date: "2019-04-07",
+      price: 45,
+    },
+    {
+      id: 625025343,
+      photographerId: 243,
+      title: "Hillside Color",
+      image: "Travel_HillsideColor.jpg",
+      likes: 85,
+      date: "2019-04-03",
+      price: 45,
+    },
+  ];
+
+  return {
+    media: [...media],
+  };
+}
+
+async function displayMedia(media) {
+  const gallerySection = document.querySelector(".gallery_section");
+
+  media.forEach((media) => {
+    const mediaModel = mediaFactory(media);
+    const userMediaDOM = mediaModel.getMediaDOM();
+    gallerySection.appendChild(userMediaDOM);
+  });
+}
+
+async function init() {
+  const { media } = await getMedia();
+  displayMedia(media);
+}
+
+init();
+
+function mediaFactory(gallery) {
+  const { image, title, likes } = gallery;
+  const photo = `assets/Mimi/${image}`;
+
+  function getMediaDOM() {
+    const article = document.createElement("article");
+
+    const image = document.createElement("img");
+    image.setAttribute("src", photo);
+
+    const title = document.createElement("title");
+    title.textContent = title;
+
+    const likes = document.createElement("likes");
+    likes.textContent = likes;
+
+    article.appendChild(image);
+    article.appendChild(title);
+    article.appendChild(likes);
+
+    return article;
+  }
+  return { photo, title, likes, getMediaDOM };
+}
