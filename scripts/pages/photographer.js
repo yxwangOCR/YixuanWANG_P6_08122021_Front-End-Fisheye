2d6c99a423af765d1dca11813ced974e9d61b04f
yxwangOCR <wang.yixuan@live.fr>
2022-01-12 13:15:13 +0100

    carrousel

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -9,7 +9,6 @@ const lightBox = document.querySelector(".slide-bground");
 const closeButton = document.getElementById("closeButton");
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
-const currentElement = document.getElementById("current_element");
 
 async function getProfilMedia() {
   let profil = [];
@@ -30,34 +29,65 @@ async function displayProfilMedia({ profil, media }) {
   const profilModel = profilFactory(profil);
   const profilDOM = profilModel.getProfilCardDOM();
   profilSection.appendChild(profilDOM);
-
   media.forEach((media) => {
     const mediaModel = mediaFactory(media);
     const mediaDOM = mediaModel.getMediaCardDOM();
-    const heart = mediaDOM.getElementsByClassName("heartIcon")[0]; //
+    const heart = mediaDOM.getElementsByClassName("heartIcon")[0];
     heart.addEventListener("click", addLikes);
-    //const getSlide = document.querySelectorAll("img"); //fonctionne pas, voir mediaFactory : ligne 21 + 29
-    //getSlide.addEventListener("click", launchSlider());
     mediaSection.appendChild(mediaDOM);
     likesCounter += media.likes; //calcul du compteur de like pour chaque fois qu'il affiche un media, il récupère son nombre de like.
+    //Affichage des 2 valeurs : price + likes
+    priceHolder.innerHTML = profil.price + " €/Jour";
+    counterHolder.innerHTML = likesCounter; // 1111111111 => pour afficher
 
     //Carousel:
     const galleryElements = mediaDOM.querySelectorAll(".gallery");
+    const currentImage = document.getElementById("current_image");
+    const currentVideo = document.getElementById("current_video");
     galleryElements.forEach((element) => {
       element.addEventListener("click", (event) => {
-        event.preventDefault();
-        launchSlider();
-        //lightBox.style.display = "block"; // ne fonctionne pas
         const getSrc = element.getAttribute("src");
         console.log(getSrc);
-        currentElement.src = element.getAttribute("src"); // image or video src same as current element
+        function getCurrentMedia() {
+          const imageFormat = `https?:\/\/.*\.(?:png|jpg)`;
+          const videoFormat = `https?.*?\.mp4`;
+
+          for (let i = 0; i < galleryElements.length; i++) {
+            if (galleryElements[i].src.match(imageFormat)) {
+              currentVideo.style.display = "none"; // hide <video>
+              currentImage.style.display = "block";
+              currentImage.src = getSrc; // image src same as current image
+            } else if (galleryElements[i].src.match(videoFormat)) {
+              currentImage.style.display = "none"; // hide <image>
+              currentVideo.style.display = "block";
+              currentVideo.src = getSrc; // video src same as current video
+              currentVideo.setAttribute("controls", "controls");
+            } else {
+              console.log("Error");
+            }
+          }
+        }
+        event.preventDefault();
+        getCurrentMedia();
+        launchSlider();
       });
     });
   });
-  //Affichage des 2 valeurs : price + likes
-  priceHolder.innerHTML = profil.price + " €/Jour";
-  counterHolder.innerHTML = likesCounter; // 1111111111 => pour afficher
 }
+
+//Launch Slides:
+const launchSlider = function () {
+  lightBox.style.display = "block";
+};
+
+//Close Slides:
+const closeSlider = function () {
+  lightBox.style.display = "none";
+};
+closeButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  closeSlider();
+});
 
 async function init() {
   const data = await getProfilMedia();
@@ -69,109 +99,5 @@ async function addLikes() {
   likesCounter += 1;
   counterHolder.innerHTML = likesCounter; // 2222222222 => pour calculer
 }
-//addLikes();
 
-//Launch Slides:
-const launchSlider = function () {
-  lightBox.style.display = "block";
-};
-
-function nextSlide() {
-  //console.log("next");
-}
-
-function previsousSlide() {
-  //console.log("previous");
-}
-
-previousButton.addEventListener("click", previsousSlide());
-nextButton.addEventListener("click", nextSlide());
-
-//Close Slides:
-const closeSlider = function () {
-  lightBox.style.display = "none";
-};
-closeButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  closeSlider();
-});
-
-// Dropdown List:
-/* Native HTML method: 
-function getDropdownList() {
-  let selectValue = document.getElementById("dropdown").value;
-  console.log(selectValue);
-}
-getDropdownList();
-*/
-
-function DropDown(dropdownElement) {
-  const [toggler, menu] = dropdownElement.children;
-
-  // change value of menu when click:
-  const setValue = (item) => {
-    const value = item.textContent;
-    toggler.textContent = value;
-    this.value = value;
-    this.toggle(false);
-    dropdownElement.dispatchEvent(new Event("changeValue")); //向一个指定的事件目标派发一个事件,  并以合适的顺序同步调用目标元素相关的事件处理函数。
-  };
-
-  // hide the menu when click:
-  // check if the dropdown element does not contain the event => false:
-  const clickOut = (e) => {
-    if (!dropdownElement.contains(e.target)) {
-      this.toggle(false);
-    }
-
-    /* 
-    if (!dropdownElement) {
-      return document.removeEventListener("click", clickOut);
-    }
-    */
-  };
-  document.addEventListener("click", clickOut);
-
-  toggler.addEventListener("click", () => this.toggle());
-  [...menu.children].forEach((item) => {
-    item.addEventListener("click", () => setValue(item));
-  });
-
-  this.element = dropdownElement;
-  this.value = toggler.textContent;
-  this.toggle = (expand = null) => {
-    expand =
-      expand === null ? menu.getAttribute("aria-expanded") !== "true" : expand;
-    menu.getAttribute("aria-expanded", expand);
-
-    // when expand menu :
-    if (expand) {
-      toggler.classList.add("active");
-      //menu.children[0].focus(); // focus on first item on the menu;
-      dropdownElement.dispatchEvent(new Event("opened"));
-      document.removeEventListener("click", clickOut);
-    } else {
-      toggler.classList.remove("active");
-      dropdownElement.dispatchEvent(new Event("closed"));
-      document.removeEventListener("click", clickOut);
-    }
-  };
-}
-
-const dropdown = new DropDown(document.querySelector(".dropdown"));
-//console.log(dropdown.value);
-dropdown.element.addEventListener("changeValue", () => {
-  console.log("changeValue", dropdown.value);
-});
-
-dropdown.element.addEventListener("opened", () => {
-  console.log("opened", dropdown.value);
-});
-
-dropdown.element.addEventListener("closed", () => {
-  console.log("closed", dropdown.value);
-});
-
-dropdown.toggle();
-
-init(); //
+init();
